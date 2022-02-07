e3e8abc5975064c3d1f6fa541a5cc814556acd29
braiso-22 <braisfv22@gmail.com>
2022-02-07 18:01:35 +0100

    Si gameOver para el juego empezado

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 10;
+    private PlayerController playerControllerScript;
+    void Start()
+    {
+        playerControllerScript =
+        GameObject.Find("Player").GetComponent<PlayerController>();
+
+    }
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
     }
 }
