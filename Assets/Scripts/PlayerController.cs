65bab059981350be9a09312dc2c1a9e16403d0fc
braiso-22 <braisfv22@gmail.com>
2022-02-08 16:58:56 +0100

    Doble salto implementado

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver = false;
+    private int saltosPosibles = 2;
     private Animator playerAnim;
     public ParticleSystem explosionParticle;
     public ParticleSystem dirtParticle;
@@ -26,14 +27,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        if (Input.GetKeyDown(KeyCode.Space) && !gameOver && saltosPosibles > 0)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
             dirtParticle.Stop();
             playerAudio.PlayOneShot(jumpSound, 1.0f);
+            saltosPosibles--;
         }
+        if (saltosPosibles <= 0 && isOnGround) saltosPosibles = 2;
     }
     private void OnCollisionEnter(Collision collision)
     {
@@ -45,7 +48,7 @@ public class PlayerController : MonoBehaviour
         else if (collision.gameObject.CompareTag("obstacle"))
         {
             gameOver = true;
-            playerAudio.PlayOneShot(crashSound,1.0f);
+            playerAudio.PlayOneShot(crashSound, 1.0f);
             Debug.Log("Game Over!");
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
